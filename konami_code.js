18067664262d0b22e83eaab36f61ca8416a98cf3
KatherineMurr <katherinemurr@yahoo.com>
2018-07-30 14:07:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,9 +8,58 @@ const codes = [
   "ArrowLeft",
   "ArrowRight",
   "b",
-  "a"
-];
+  "a"];
+  function init(window) {
+    
+  if (window.addEventListener) {
+  var keys = []
+  
+  const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"]; }
+  
+  window.addEventListener("keydown", function(e){
+        keys.push(e.keyCode);
+        if (keys.toString().indexOf(codes) >= 0) 
+            keys = [];
+            });
+    } true; 
+};
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+// let index = 0 ;
 
-function init() {
-  // your code here
-}
+//function init(codes) {
+  //element.addEventListener("codes", function(codes){ alert("Hello World!"); });
+  //console.log("finally")
+
+  
+  //const key = e.key;
+  
+  //if (key === codes[index]) {
+    //index ++;
+    //if (index === codes.length) {
+      //alert("Hurray!");
+      
+      //index = 0;
+    //}
+  //} else {
+    //index = 0;
+  }
